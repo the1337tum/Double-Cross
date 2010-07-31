41653b6ab83b2c05b74bedc8728a106a0cbb9e5d
Tum <tim.blackburn09@gmail.com>
2010-07-31 15:59:59 +1200

    lots of fatal errors in input classes

---
+++ input.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int readFile(const char * name); 
+
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "input.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return readFile(*argv);
+}
