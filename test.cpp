165618cf07ccb81705809234f6efbfac331ed0e4
Matthew Heard <mheard@oucs1178.otago.ac.nz>
2010-07-31 17:16:33 +1200

    Testing CharMap

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "input.h"
+//#include "input.h"
+#include "CharMap.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    return readFile(*argv);
+//    return readFile(*argv);
+	
+//	CharMap *map = new CharMap(1, 1, ...);
 }
