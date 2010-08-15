0dd25bae4f212758eca24a612089fa042e852bdb
Tum <tim.blackburn09@gmail.com>
2010-08-15 18:23:14 +1200

    Adding templates - arrayAlloc still to be tested

---
+++ data_structures/CyclicArray.hpp
@@ -0,0 +1,36 @@
+#ifndef __CyclicArray__
+#define __CyclicArray__
+
+#include <stdio>
+#include <new>
+
+/**
+ *  A wrapping fixed length cyclic array of pointers 
+ *  Intentionally kept as simple as possible
+**/
+
+using namespace std;
+
+template <typename Type, int size>
+class CyclicArray { 
+private:
+    Type *array[size];
+    int offset = 0;
+
+public:
+    const int length = size;
+
+    CyclicArray() { /* All taken care of by the template */ } 
+    ~CyclicArray() { /* All memory on the stack */ };
+   
+    // Array subscript operator - pointers are mutable
+    Type &operator[](const int index) {
+        return array[(index + offset) % length];
+    }
+    
+    cycleArray(int step) {
+        offset = (offset + step) % length;
+    }
+};
+
+#endif /* __CyclicArray__*/
---
+++ data_structures/Queue.hpp
@@ -0,0 +1,67 @@
+/*	This quality class was brought to you by Tum. */
+
+/*	A fixed length cyclic queue template. */
+
+template <typename ItemType>
+struct QueueNode {
+   ItemType data;
+   QueueNode<ItemType> *next;
+};
+
+template <typename ItemType, int size_>
+class Queue {
+private:
+	QueueNode<ItemType> *first;
+	QueueNode<ItemType> *last;
+	QueueNode<ItemType> queue[size_];
+
+public:
+	const int length = size_; // Feels like home :)
+	
+	Queue() {
+		first = NULL;
+		last = NULL;
+		for( int node = 0; node < length; node++ )
+			queue[node].next = &queue[(node + 1) % length];
+	}
+
+	~Queue() { /* No allocated memory */ }
+
+                
+	int isEmpty() { return first == NULL; }
+
+	int isFull() { return last->next == first; }
+
+	void put( ItemType item ) {
+		if( isEmpty() )
+		{
+			first = queue;
+			last = queue;
+			first->data = item;
+		} else if( isFull() ) {
+			return; 
+		} else {
+			last = last->next;
+			last->data = item;
+		}
+	}
+
+	ItemType get() {
+		QueueNode<ItemType> *result;
+
+		if( isEmpty() ) { return NULL; }
+		if( first == last ) 
+		{
+			result = first;
+			last = first = NULL;
+		} else {
+			result = first;
+			first = first->next;
+		}
+		return result->data;
+	}
+
+	ItemType peek() {
+		if(! isEmpty() ) { return first->data; }
+	}
+};
