d939a3a5f1f46e2bb9ad1642b47b5a215341f8d2
Tum <tim.blackburn09@gmail.com>
2010-08-21 19:55:29 +1200

    tinkering with display

--- Display.cpp
+++ Display.cpp
@@ -2,7 +2,7 @@
 
 using namespace Display
 
-Display(int height, int width) : height( height ), width( width ) {
+Display(int height, int width) : height(height), width(width) {
     initscr();              // Start curses mode
     nodelay(stdscr, TRUE);  // Don't wait for input on getch()
     keypad(stdscr, TRUE);   // Get function keys (F1 and arrows)
@@ -10,7 +10,7 @@ Display(int height, int width) : height( height ), width( width ) {
     noecho();               // Don't echo() for getch
 }
 
-Display() : DisplayIO( 80, 40 ) { /* Default Constructor */ }
+Display() : height(80), width(40) { /* Default Constructor */ }
 
 ~Display() {
     endwin();   // End curses mode
