dad00d09894d589509103d1c5f87cfdf4dcbf44c
Tum <tim.blackburn09@gmail.com>
2010-08-23 01:35:58 +1200

    Fill out this file, David!

--- Library/Char~HashTree.hpp
+++ Library/Char~HashTree.hpp
@@ -2,29 +2,11 @@
  * This hashes strings to binary search trees for parsing
  * options, save files and datastructures. 
  * Use in switch statement, with comparision to the stored string. 
- *
- ** The class interface for comparisions
- ** 1 for found match, 0 for failure. 
- * int matchString(char *) 
- *
- **/
+**/
 #include "BST.h"
 #include "hashFunction.h"
+#include "../Art/fileNames.h"
 
-
-extern static struct FileNames{
-    char *config = "Config.txt";
-    /**
-     * Can you give me some more examples, 
-     * alturnatively, just add the names of all 
-     * like the example above.
-    **/
-}FILE_NAMES;
-
-/**
- * Just treat this like any other datastructure
- *
-**/
 class String~HashTree {
 private:
     BinarySearchTree tree
