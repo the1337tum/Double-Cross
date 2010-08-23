eb0b9ad3cbccfe264f121001fa1623341a4988ab
Tum <tim.blackburn09@gmail.com>
2010-08-23 20:28:52 +1200

    should be working now...

--- Library/Input.cpp
+++ Library/Input.cpp
@@ -1,26 +1,35 @@
-#include "input.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "ReadFile.h"
 
-using namespace std;
+ReadFile::ReadFile (char *fileName, CharMap *charMap) { 
+    infile.open (filename);
+    int line = 0;
+    while (!infile.eof && line < MAX_BUFFER)
+	getline(infile, array_buffer[line++] = new string ());
+}
 
-int readFile(CharMap *map, const char *file_name) {
-    FILE * pFile;
-    string line;
-    ifstream myfile (file_name); 
-     
-    if (myfile.is_open()) {
-        while (! myfile.eof()) {
-            getline(myfile,line);
-            switch (line) {
-            
-            }
-        }
-        myfile.close();
-        return 1; 
-    } else {
-        cout << "Unable to open file";
-        return 0;
-    } 
+virtual ReadFile::~ReadFile (){
+    if (array_buffer[0] != NULL)
+        for (int index = 0; array_buffer[index] != NULL; index++)
+	    delete array_buffer[index];
+
+    if (FREE_ME[0] != NULL)
+        for (int index = 0; FREE_ME[index] != NULL; index++)
+	    delete FREE_ME[index];
+
+    infile.close ();
+}
+
+void ReadFile::writeCharMap (CharMap * charMap) {
+
+    int rows = array_buffer[0].size;
+    int cols = array_buffer.size ();
+    char **array = alloc_array_char (rows, cols);
+      
+    for (int row = 0; row < rows; row++)
+        for (int col = 0; col < cols; col++)
+	    array[row][col] = array_buffer[row][col];
+
+    charMap->replaceMap (array);
+    CHAR_MAPS[buffIndex++] = array;
 }
 
--- Library/Input.hpp
+++ Library/Input.hpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include <iostream.h>
 #include <fstream>
 #include <string>
 
-int readFile(const char * name); 
+#define MAX_BUFFER 1000
+
+using namespace std;
+
+/**
+ * Used for reading files into datastructures
+**/
+class ReadFile {
+private:
+    ifstream infile;
+    string *STRING_BUFFER[MAX_BUFFER];
+    int str_buff = 0;
+
+    char[MAX_BUFFER]** CHAR_MAPS;
+    int char_buff = 0;
+
+public:
+    ReadFile(char *fileName, CharMap * charMap);
+    virtual ~ReadFile();
+    void writeCharMap(CharMap * charMap);
+};
 
