5815d122352c7da3e14f2a017ba1c433ef271240
David Agnitto <agnitto@mine.(none)>
2010-08-23 15:18:49 +1200

    DONE FOR NOW

--- Art/fileNames.h
+++ Art/fileNames.h
@@ -5,10 +5,28 @@
 
 extern static struct LevelFiles{
     char *config = "Config.txt";
-    /**
-     * Can you give me some more examples, 
-     * alturnatively, just add the names of all 
-     * like the example above.
-    **/
+LEVEL1.txt;
+LEVEL2.txt;
+LEVEL3.txt;
+LEVEL4.txt;
+LEVEL5.txt;
+FINAL_STATIC.txt;
+LEVEL1_SPLASH.txt;
+LEVEL2_SPLASH.txt;
+LEVEL3_SPLASH.txt;
+LEVEL4_SPLASH.txt;
+LEVEL5_SPLASH.txt;
+FINAL_SPLASH.txt;
+LEVEL1_BADDIES.txt;
+LEVEL1_BADDIES_STRIKE.txt;
+LEVEL2_BADDIES.txt;
+LEVEL3_BADDIES.txt;
+LEVEL4_BADDIES.txt;
+LEVEL5_BADDIES.txt;
+FINAL_BADDIES.txt;
+MECCA.txt;
+DAGGER_BATON_RUN.txt;
+DAGGER_BATON_STRIKE.txt;
+DAGGER_GUN.txt
 } LEVEL_FILES;
 
